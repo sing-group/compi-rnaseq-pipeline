ac3c43c738e5cb6b7d81de935e7b217b984428c2
Hugo López-Fernández <hlfernandez@uvigo.es>
2025-01-17 11:42:02 +0100

    Fixes regex for group filtering

--- tasks/prepare_deas.sh
+++ tasks/prepare_deas.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 tail -n +2 ${working_dir}/${contrasts_file} | while IFS= read -r line; do
-    word_grep=$(printf "%s" "$line" | sed 's#\t#|#')
-    dir=$(echo ${word_grep} | sed 's#|#_#')
+    dir=$(printf "%s" "$line" | sed 's#\t#_#')
     reference=$(printf "%s" "$line" | cut -f1)
+    other=$(printf "%s" "$line" | cut -f2)
 
     echo -e "Processing contrasts line: $line"
     echo -e "\tReference: $reference"
+    echo -e "\tVersus: $other"
 
     mkdir -p ${working_dir}/${dea_dir}/${dir}
 
     head -1 ${working_dir}/${samples_dir}/${metadata_file} > ${working_dir}/${dea_dir}/${dir}/metadata.tsv
-    cat ${working_dir}/${samples_dir}/${metadata_file} |grep -E "(^|[[:space:],])${word_grep}([[:space:],]|$)" >> ${working_dir}/${dea_dir}/${dir}/metadata.tsv
+
+    cat ${working_dir}/${samples_dir}/${metadata_file} | grep -E -e "(^|[[:space:],])$reference([[:space:],]|$)" -e "(^|[[:space:],])$other([[:space:],]|$)" >> ${working_dir}/${dea_dir}/${dir}/metadata.tsv
+    
     echo ${reference} > ${working_dir}/${dea_dir}/${dir}/reference.txt
 
-    # ${scripts_dir}/join_all.sh ${working_dir}/${samples_htseqcount_dir} ${working_dir}/${dea_dir}/${dir}/counts.tsv ${working_dir}/${dea_dir}/${dir}/metadata.tsv
-    # don't join all files because we need to use the batch corrected all counts file
-    # filter out the metadata file by condition (word_grep) and then filter out the all counts file by the samples remaining in the metadata file
     python3 ${scripts_dir}/extract_subsample.py ${working_dir}/${dea_dir}/${dir}/metadata.tsv ${working_dir}/${all_counts_dir}/counts.tsv ${working_dir}/${dea_dir}/${dir}/counts.tsv
 done
