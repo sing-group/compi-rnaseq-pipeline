64428a4a4c7921cf8d3984ba238623248454772a
Hugo López-Fernández <hlfernandez@uvigo.es>
2025-01-17 11:03:45 +0100

    Allows choosing between htseq and featurecounts

--- tasks/join_all.sh
+++ tasks/join_all.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# ${scripts_dir}/join_all.sh ${working_dir}/${samples_feature_counts_dir} ${working_dir}/compi/counts.tsv ${working_dir}/${samples_dir}/${metadata_file}
-
-${scripts_dir}/join_all.sh ${working_dir}/${samples_htseqcount_dir} ${working_dir}/${all_counts_dir}/counts.tsv ${working_dir}/${samples_dir}/${metadata_file} 
+if [ "$counts_method" == "htseq" ]; then
+    ${scripts_dir}/join_all.sh ${working_dir}/${samples_htseqcount_dir} ${working_dir}/${all_counts_dir}/counts.tsv ${working_dir}/${samples_dir}/${metadata_file}
+elif [ "$counts_method" == "featurecounts" ]; then
+    ${scripts_dir}/join_all.sh ${working_dir}/${samples_feature_counts_dir} ${working_dir}/${all_counts_dir}/counts.tsv ${working_dir}/${samples_dir}/${metadata_file}
+else
+    echo "Error: counts_method must be either 'htseq' or 'featurecounts'"
+    exit -1
+fi
