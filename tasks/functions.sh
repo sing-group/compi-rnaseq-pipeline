1ffff0939b13afbeb1ce546025f2cc09c4ac3f02
Hugo López-Fernández <hlfernandez@uvigo.es>
2025-02-21 12:02:59 +0100

    Avoids rm failure

--- tasks/functions.sh
+++ tasks/functions.sh
@@ -3,7 +3,7 @@ function cp_and_lock {
 # $1 : script to copy  # $2 : task name  # $3 : cp target directory
 	(
 	flock 200 || echo "[task "${2}"]: ${2} lock file is already locked, cp omitted."
-	rm ${3}
+	rm -f ${3}
 	cp -n ${1} ${3}
 	) 200>/var/lock/${2}.lock
 }
