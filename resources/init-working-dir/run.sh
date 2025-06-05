58cfd28641871aa705e57bf328ecf386d1f116e0
Hugo López-Fernández <hlfernandez@uvigo.es>
2025-06-05 16:08:20 +0200

    Uses version in run.sh

--- resources/init-working-dir/run.sh
+++ resources/init-working-dir/run.sh
@@ -10,7 +10,7 @@ function get_compi_parameter {
     cat "${1}" | grep "${2}=" | cut -d'=' -f2
 }
 
-COMPI_RNA_SEQ_VERSION=${COMPI_RNA_SEQ_VERSION-latest}
+COMPI_RNA_SEQ_VERSION=${COMPI_RNA_SEQ_VERSION-2.2.0-SNAPSHOT}
 
 FULL_COMPI_PARAMS_FILE=$1
 ADDITIONAL_COMPI_PARAMS="${2:--num-tasks 5}"
